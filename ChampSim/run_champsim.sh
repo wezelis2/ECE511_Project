ca2e7067cbf0e798a1d5f02fba9d7c68745895a4
wezelis2 <wezelis2@illinois.edu>
2021-11-13 21:18:21 -0600

    small fix

--- ChampSim/run_champsim.sh
+++ ChampSim/run_champsim.sh
@@ -43,4 +43,7 @@ fi
 
 mkdir -p results_${N_SIM}M
 (./bin/${BINARY} -warmup_instructions ${N_WARM}000000 -simulation_instructions ${N_SIM}000000 ${OPTION} -traces ${TRACE_DIR}/${TRACE}) &> results_${N_SIM}M/${TRACE}-${BINARY}${OPTION}.txt
-exit $?
+
+retVal=$?
+echo "Returning $retVal"
+exit $retVal
