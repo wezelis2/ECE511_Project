813f24398c8195c0a789fa4dc08ae21d65848fb7
wezelis2 <wezelis2@illinois.edu>
2021-11-13 21:15:55 -0600

    small fix

--- ChampSim/run_champsim.sh
+++ ChampSim/run_champsim.sh
@@ -43,3 +43,4 @@ fi
 
 mkdir -p results_${N_SIM}M
 (./bin/${BINARY} -warmup_instructions ${N_WARM}000000 -simulation_instructions ${N_SIM}000000 ${OPTION} -traces ${TRACE_DIR}/${TRACE}) &> results_${N_SIM}M/${TRACE}-${BINARY}${OPTION}.txt
+exit $?
