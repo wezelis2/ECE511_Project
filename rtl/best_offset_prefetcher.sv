55975039cc610b475d7eda1033b5cba91a4ed59c
Eric Dong <ericd3@illinois.edu>
2021-11-14 22:21:18 -0600

    working cop, needs verifying

--- rtl/best_offset_prefetcher.sv
+++ rtl/best_offset_prefetcher.sv
@@ -1,3 +1,6 @@
+`define NOFFSETS 			46
+`define OFFSET_MAX			40
+
 module best_offset_prefetcher #(
 	parameter 	WIDTH 			= 64,
 	parameter 	UP_NUM_SET 		= 8,
@@ -12,10 +15,8 @@ module best_offset_prefetcher #(
 	input 		logic 					lo_ready_i,				// lower level cache ready to take requests
 		
 	output 		logic 	[WIDTH - 1:0]	lo_prefetch_address_o,	// lower level cache prefetch address
-	output 		logic 					lo_prefetch_valid_o,	// lower level cache prefetch valid
+	output 		logic 					lo_prefetch_valid_o	// lower level cache prefetch valid
 );
-
-	localparam 	int 	NOFFSETS 			=  	46;
 	localparam 	int 	DEFAULT_OFFSET 		=  	1;
 	localparam 	int 	SCORE_MAX 			=  	31;
 	localparam 	int 	ROUND_MAX 			=  	100;
@@ -31,22 +32,8 @@ module best_offset_prefetcher #(
 	localparam 	int 	LOW_SCORE 			=  	20;
 	localparam 	int 	BAD_SCORE 			=  	1; //(knob_small_llc)	? 10 : 1;
 	// localparam 	int 	BANDWIDTH 			=  	(knob_low_bandwidth)? 64 : 16;
-	localparam 	int 	OFFSET_MAX 			= 	40;
 	localparam 	int 	LINE_SIZE 			= 	256;
 	localparam 	int 	LOGLINE				= 	6;
-
-	localparam 	int 	OFFSET[NOFFSETS-1:0]= 	{	1,		-1,		2,		-2,		
-													3,		-3,		4,		-4,		
-													5,		-5,		6,		-6,		
-													7,		-7,		8,		-8,		
-													9,		-9,		10,		-10,	
-													11,		-11,	12,		-12,	
-													13,		-13,	14,		-14,	
-													15,		-15,	16,		-16,	
-													18,		-18,	20,		-20,	
-													24,		-24,	30,		-30,	
-													32,		-32,	36,		-36,	
-													OFFSET_MAX,		-OFFSET_MAX};
 
 	//######################################################################################
 	//										ENUMS AND STRUCTS
@@ -58,25 +45,26 @@ module best_offset_prefetcher #(
 	//										LOGIC DECLARATION
 	//######################################################################################
 
-	logic 	signed 	[$clog2(OFFSET_MAX) - 1:0] 		prefetch_offset;
+	logic 	signed 	[$clog2(`OFFSET_MAX) - 1:0] 	prefetch_offset;
 	logic 			[$clog2(SCORE_MAX) 	- 1:0]	 	prefetch_score;
 	// logic 		 	[RRTAG - 1:0]					rr_table 			[1:0] [1<<RRINDEX - 1:0];
 	logic 											prefetched_table 	[UP_NUM_SET] [UP_NUM_ASSO];
-	logic 			[$clog2(SCORE_MAX) - 1:0] 		score 				[NOFFSETS - 1:0];
+	logic 			[$clog2(SCORE_MAX) - 1:0] 		score 				[`NOFFSETS - 1:0];
 	logic 			[$clog2(SCORE_MAX) - 1:0] 		curr_max_score, next_max_score;
-	logic 			[$clog2(NOFFSETS)  - 1:0] 		best_offset_idx, next_best_offset_idx;
+	logic 			[$clog2(`NOFFSETS)  - 1:0] 		best_offset_idx, next_best_offset_idx;
 	logic 			[$clog2(ROUND_MAX) - 1:0]		curr_round;
-	logic 			[$clog2(NOFFSETS)  - 1:0]		curr_offset_idx;	// p in original c code
-
+	logic 			[$clog2(`NOFFSETS)  - 1:0]		curr_offset_idx;	// p in original c code
+	int 											OFFSET[`NOFFSETS-1:0];
 
 	// recent requests table signals
-	logic 											read_left, 	read_right;
-	logic 											write_left, write_right;
-	logic 			[WIDTH - 1:0] 					data_left, 	data_right;
-	logic 											hit_left, 	hit_right;
-	logic 											data_left, 	data_right;
-	logic 											valid_left, valid_right;
+	logic 											read_left, 			read_right;
+	logic 											write_left, 		write_right;
+	logic 			[WIDTH - 1:0] 					data_left, 			data_right;
+	logic 											hit_left, 			hit_right;
+	logic 											data_left_out, 		data_right_out;
+	logic 											valid_left, 		valid_right;
 	logic 											rr_hit;
+	logic 			[WIDTH - 1:0]					read_address_left, 	read_address_right;		
 
 	// delay queue signals
 	logic 											delay_queue_enq;
@@ -91,31 +79,45 @@ module best_offset_prefetcher #(
 	//######################################################################################
 	assign 			next_max_score 			= 	score[curr_offset_idx] + 1 >= curr_max_score ? score[curr_offset_idx] + 1 : curr_max_score;
 	assign 			next_best_offset_idx	= 	score[curr_offset_idx] + 1 >= curr_max_score ? curr_offset_idx : best_offset_idx;
+	assign 			OFFSET 					= 	'{	1,		-1,		2,		-2,		
+													3,		-3,		4,		-4,		
+													5,		-5,		6,		-6,		
+													7,		-7,		8,		-8,		
+													9,		-9,		10,		-10,	
+													11,		-11,	12,		-12,	
+													13,		-13,	14,		-14,	
+													15,		-15,	16,		-16,	
+													18,		-18,	20,		-20,	
+													24,		-24,	30,		-30,	
+													32,		-32,	36,		-36,	
+													`OFFSET_MAX,	-`OFFSET_MAX};
 
 	//######################################################################################
 	//										MODULE DECLARATION
 	//######################################################################################
-	bank rr_table_left #(WIDTH, RRTAG, RRINDEX, 1 << RRINDEX, LEFT) (
+	bank #(WIDTH, RRTAG, RRINDEX, 1 << RRINDEX, LEFT) rr_table_left (
 		.read_i(read_left),
 		.write_i(write_left),
 		.data_i(data_left),
+		.read_address_i(read_address_left),
 		.hit_o(hit_left),
-		.data_o(data_left),
+		.data_o(data_left_out),
 		.valid_o(valid_left),
 		.*
 	);
 
-	bank rr_table_right #(WIDTH, RRTAG, RRINDEX, 1 << RRINDEX, RIGHT) (
+	bank #(WIDTH, RRTAG, RRINDEX, 1 << RRINDEX, RIGHT) rr_table_right (
 		.read_i(read_right),
 		.write_i(write_right),
 		.data_i(data_right),
+		.read_address_i(read_address_right),
 		.hit_o(hit_right),
-		.data_o(data_right),
+		.data_o(data_right_out),
 		.valid_o(valid_right),
 		.*
 	);
 
-	circular_queue delay_queue #(WIDTH, DELAY) (
+	circular_queue #(WIDTH, DELAY) delay_queue (
 		.enq(delay_queue_enq),
 		.deq(1'b1),
 		.in(delay_queue_in),
@@ -139,7 +141,7 @@ module best_offset_prefetcher #(
 	endtask
 
 	task reset_offset_score();
-		for(int i = 0; i < NOFFSETS; i++) begin 
+		for(int i = 0; i < `NOFFSETS; i++) begin 
 			score[i] 	<= '0;
 		end 
 
@@ -164,56 +166,63 @@ module best_offset_prefetcher #(
 		write_right			= '0;
 		data_left			= '0;
 		data_right			= '0;
-		delay_queue_enq		= '0;
-		delay_queue_in		= '0;
+		// delay_queue_enq		= '0;
+		// delay_queue_in		= '0;
 	endfunction
 
 	//######################################################################################
 	//									RR AND DQ TASKS
 	//######################################################################################
 
-	task rr_table_insert(logic [WIDTH - 1:0] data, rr_side side);
+	function void rr_table_insert(logic [WIDTH - 1:0] data, rr_side side);
 		unique case(side)
 			LEFT: 	begin 
-				write_left	<= 1'b1;
-				data_left	<= data;
+				write_right	= 0;
+				write_left	= 1'b1;
+				data_left	= data;
 			end 
 
 			RIGHT:	begin 
-				write_right	<= 1'b1;
-				data_right	<= data;
+				write_right	= 1'b1;
+				write_left	= 0;
+				data_right	= data;
 			end 
 
-			default:;
+			default: begin 
+				write_right	= 0;
+				write_left	= 0;
+				data_right	= '0;
+			end 
 		endcase
-	endtask
+	endfunction
 
 	task delay_queue_push(logic [WIDTH - 1:0] data);
 		delay_queue_enq 	<= 1'b1;
 		delay_queue_in 		<= data;
 	endtask
 
-	task dq_pop_rr_left_insert();
+	function void dq_pop_rr_left_insert();
 		if(delay_queue_ready) begin 
-			rr_table_insert(delay_queue_out, LEFT)
+			rr_table_insert(delay_queue_out, LEFT);
+		end else begin
+			write_right	= 0;
+			write_left	= 0;
+			data_right	= '0;
 		end
-	endtask
+	endfunction
 
 	//######################################################################################
 	//									MISC TASKS
 	//######################################################################################
 
 	task learn_best_offset(logic [WIDTH - 1:0] address);
-		logic 	signed 	[$clog2(OFFSET_MAX) - 1:0] 	test_offset = OFFSET[curr_offset_idx];
-		logic 			[WIDTH - 1:0] 				test_addr 	= address - test_offset; 
-
 		if (rr_hit) begin 
 			score[curr_offset_idx] 	<= score[curr_offset_idx] + 1 == SCORE_MAX ? SCORE_MAX : score[curr_offset_idx] + 1;
 			curr_max_score 			<= next_max_score;
 			best_offset_idx 		<= next_best_offset_idx;
 		end 
 
-		if (curr_offset_idx == (NOFFSETS - 1)) begin
+		if (curr_offset_idx == (`NOFFSETS - 1)) begin
 			curr_round 				<= curr_round + 1;
 
 			if(next_max_score == SCORE_MAX || (curr_round + 1) == ROUND_MAX) begin 
@@ -227,10 +236,10 @@ module best_offset_prefetcher #(
 			end 
 		end 
 
-		curr_offset_idx 			<= curr_offset_idx == NOFFSETS - 1 ? 0 : curr_offset_idx + 1;
+		curr_offset_idx 			<= curr_offset_idx == `NOFFSETS - 1 ? 0 : curr_offset_idx + 1;
 	endtask
 
-	task issue_prefetch(logic [WIDTH - 1:0] address, logic [$clog2(OFFSET_MAX) - 1:0] offset);
+	task issue_prefetch(logic [WIDTH - 1:0] address, logic [$clog2(`OFFSET_MAX) - 1:0] offset);
 		delay_queue_push(address);
 		if(offset != 0 && lo_ready_i) begin 
 			lo_prefetch_address_o	<= address + offset;
@@ -242,14 +251,14 @@ module best_offset_prefetcher #(
 		logic 	[$clog2(UP_NUM_SET)  - 1:0] set 			= get_up_set(address);
 		logic 	[$clog2(UP_NUM_ASSO) - 1:0] way 			= get_up_way(address);
 		logic 								prefetched 		= 0;
-		logic 								prefetch_issued	= offset != 0 && lo_ready_i;
+		logic 								prefetch_issued	= prefetch_offset != 0 && lo_ready_i;
 
 		if (hit) begin 
 			prefetched 										<= prefetched_table[set][way];
 			prefetched_table[set][way] 						<= 0;
 		end
 
-		dq_pop_rr_left_insert();
+		// dq_pop_rr_left_insert();
 
 		if (~hit | prefetched_table[set][way]) begin 
 			learn_best_offset(address);
@@ -265,9 +274,9 @@ module best_offset_prefetcher #(
 
 		prefetched_table[set][way] 							<= prefetch_bit;
 
-		if (prefetch_bit || prefetch_offset == 0) begin 
-			rr_table_insert((address >> LOGLINE) - prefetch_offset, RIGHT);
-		end 
+		// if (prefetch_bit || prefetch_offset == 0) begin 
+		// 	rr_table_insert((address >> LOGLINE) - prefetch_offset, RIGHT);
+		// end 
 
 	endtask
 
@@ -283,13 +292,13 @@ module best_offset_prefetcher #(
 		return 0;
 	endfunction
 
-	function logic rr_check_hit (logic [WIDTH - 1:0] address, logic valid);
-		read_left 	= 1;
-		read_right	= 1;
-		data_left	= address;
-		data_right	= address;
-
-		return valid & (hit_left | hit_right);
+	function void rr_check_hit (logic [WIDTH - 1:0] address, logic valid);
+		read_left 			= valid;
+		read_right			= valid;
+		read_address_left	= address;
+		read_address_right	= address;
+		
+		rr_hit 		= valid & (hit_left | hit_right);
 	endfunction
 
 	//######################################################################################
@@ -301,7 +310,7 @@ module best_offset_prefetcher #(
 		if (rst) begin
 			reset();
 		end else begin
-			prefetcher_operate(up_address_i, ~up_miss_i & up_valid_i)
+			prefetcher_operate(up_address_i, ~up_miss_i & up_valid_i);
 			if (~up_miss_i & up_valid_i) begin 
 				fill_cache(up_address_i, up_prefetched_i & up_valid_i);
 			end 
@@ -310,7 +319,11 @@ module best_offset_prefetcher #(
 
 	always_comb begin 
 		set_defaults();
-		rr_hit = rr_check_hit(up_address_i, up_valid_i);
+		rr_check_hit(up_address_i - signed'(OFFSET[curr_offset_idx]), up_valid_i);
+		dq_pop_rr_left_insert();
+		if (up_prefetched_i || prefetch_offset == 0) begin 
+			rr_table_insert(up_address_i - prefetch_offset, RIGHT);
+		end 
 	end
 
 endmodule
